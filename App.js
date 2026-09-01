4564a08e27dff981b71b5950956d4682208072d3
SJanagan <janagan092@gmail.com>
2026-09-01 15:04:57 +0530

    new changes

---
+++ App.js
@@ -0,0 +1,17 @@
+    const heading1 =React.createElement("h1", {id : "title"}, "Heading 1");
+        const heading2 = React.createElement("h2",{id : "title"},"heading 2");
+        const heading = React.createElement("h1", {}, "Namaste Everyone!");
+        console.log(heading);
+        
+        const container = React.createElement("div", {
+            id: "container"
+        },
+        [heading1, heading2]
+    )
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        console.log(root);
+        
+        //passing a react element inside the root
+
+        //async defer
+        root.render(container);
