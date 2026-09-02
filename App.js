f6ee1808670266ede330a6147f589cbc92bdc526
SJanagan <janagan092@gmail.com>
2026-09-02 12:13:02 +0530

    learned parcel and npm

--- App.js
+++ App.js
@@ -1,4 +1,29 @@
-    const heading1 =React.createElement("h1", {id : "title"}, "Heading 1");
+/*
+    HMR - Hot module replacement
+    File Watcher algorithm  - C++
+    BUNDLING
+    MINIFY
+    Cleaning our code 
+    Dev and Production Build
+    Super fast build algorithm
+    Image Optimization
+    caching while development
+    Consistent Hashing Algorithms
+    Zero config bundling
+    Tree Shaking - Removing unused code
+
+
+
+    Transitive Dependencies
+
+*/   
+   
+   
+   import React from "react";
+    import ReactDOM from "react-dom/client";
+   
+   
+   const heading1 =React.createElement("h1", {id : "title"}, "Heading 1");
         const heading2 = React.createElement("h2",{id : "title"},"heading 2");
         const heading = React.createElement("h1", {}, "Namaste Everyone!");
         console.log(heading);
